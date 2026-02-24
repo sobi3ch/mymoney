910c4981235dae704f5d56192b56439fcd015c33
Piotr Sobieszczański <psobiesz@pmintl.net>
2026-02-24 19:17:13 +0100

    Update appHeader style and name of subtitle

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,5 +7,5 @@ const props = defineProps<{
 
 <template>
   <h1 class="my-4 font-bold text-4xl">{{ props.appTitle }}</h1>
-  <h2 class="my-2 italic text-xl">{{ props.appSubtitle }}</h2>
+  <h2 class="my-2 italic text-md">{{ props.appSubtitle }}</h2>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 const appTitle = 'Auraa'
-const appSubtitle = 'Budżet domowy'
+const appSubtitle = 'Twój domowy budżet'
 document.title = appTitle
 createApp(App).provide('appTitle', appTitle).provide('appSubtitle', appSubtitle).mount('#app')
