825074146576ae0e0433f35e32917fa379cd2141
Piotr Sobieszczański <psobiesz@pmintl.net>
2026-02-24 19:11:59 +0100

    Restyle transactionList to tailwind

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -10,37 +10,22 @@ defineProps({
 </script>
 
 <template>
-  <div id="transaction-list">
-    <p>HISTORY</p>
-    <ul v-if="transactions.length > 0">
-      <li v-for="transaction in transactions" :key="transaction.id">
+  <div id="transaction-list" class="rounded bg-white p-4 mb-6 border-1 border-gray-300">
+    <p class="py-2 rounded-sm font-bold uppercase">historia</p>
+    <ul v-if="transactions.length > 0" class="">
+      <li
+        v-for="transaction in transactions"
+        :key="transaction.id"
+        :class="[
+          'border-r-8 p-2 my-2 flex justify-between last:pb-0',
+          transaction.amount > 0 ? 'border-green-500' : 'border-red-500',
+        ]"
+      >
         <span>{{ transaction.description }}</span>
-        <span>{{ transaction.amount > 0 ? '+' : '' }}{{ transaction.amount }} PLN</span>
+        <span class="px-3"
+          >{{ transaction.amount > 0 ? '+' : '' }}{{ transaction.amount }} PLN</span
+        >
       </li>
     </ul>
   </div>
 </template>
-
-<style scoped>
-#transaction-list {
-  background-color: #fff;
-  padding: 20px;
-  text-align: left;
-  margin-bottom: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
-#transaction-list ul {
-  list-style-type: none;
-  padding: 0;
-}
-#transaction-list li {
-  display: flex;
-  justify-content: space-between;
-  padding: 10px 0;
-  border-bottom: 1px solid #eee;
-}
-#transaction-list li:last-child {
-  border-bottom: none;
-}
-</style>
