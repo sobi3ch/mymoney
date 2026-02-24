77c3916dc5ce484f093804966f5f49b05ec92dfe
Piotr Sobieszczański <psobiesz@pmintl.net>
2026-02-24 14:06:13 +0100

    Add Tailwind

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,6 +6,6 @@ const props = defineProps<{
 </script>
 
 <template>
-  <h1>{{ props.appTitle }}</h1>
-  <h2>{{ props.appSubtitle }}</h2>
+  <h1 class="my-4 font-bold text-4xl">{{ props.appTitle }}</h1>
+  <h2 class="my-2 italic text-xl">{{ props.appSubtitle }}</h2>
 </template>
